b8e6e98f3dd16aae1a93e81a8f68c280da3d966f
Christian Holes <christian.holes@starwoodvo.com>
2017-02-02 11:44:44 -0500

    edited the index.js file to provide a message

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 require('./dist/multipleDatePicker');
 module.exports = 'multipleDatePicker';
+
+exports.printMsg = function() {
+  console.log("This is a message from the index.js file -- edit as needed");
+}
