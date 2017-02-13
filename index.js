9b2be8fdef5416b0114d1055b01364c4724b36cf
Christian Holes <christian.holes@starwoodvo.com>
2017-02-13 14:43:52 -0500

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// function __export(m) {
-//     for (let p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-// }
-// __export(require('./dist/multiple-date-picker.module'));
+function __export(m) {
+    for (let p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./dist/multiple-date-picker.module'));
 
-exports.MultipleDatePickerModule = require('./dist/multiple-date-picker.module').MultipleDatePickerModule;
+// exports.MultipleDatePickerModule = require('./dist/multiple-date-picker.module').MultipleDatePickerModule;
 
 exports.printMsg = function() {
   console.log("This is a message from the index.js file -- edit as needed");
