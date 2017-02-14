b2772af020329d747c7b315eb5770a18da1d1ab0
Christian Holes <christian.holes@starwoodvo.com>
2017-02-13 20:19:40 -0500

    hot fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // function __export(m) {
 //     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 // }
-__export(require('./dist/multiple-date-picker.module'));
+exports = require('./dist/multiple-date-picker.module');
 
 // // exports.MultipleDatePickerModule = require('./dist/multiple-date-picker.module').MultipleDatePickerModule;
 
